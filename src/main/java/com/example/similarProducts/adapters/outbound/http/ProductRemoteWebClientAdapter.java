35b45a1ddace3969f9ef1ddda40252849cbc74bd
samuel-pnj <samuelperezgamez@outlook.com>
2025-10-31 11:05:47 +0100

    feat(adapter): implement outbound HTTP clients for product and similar IDs

--- src/main/java/com/example/similarProducts/adapters/outbound/http/ProductRemoteWebClientAdapter.java
+++ src/main/java/com/example/similarProducts/adapters/outbound/http/ProductRemoteWebClientAdapter.java
@@ -1,5 +1,7 @@
 package com.example.similarProducts.adapters.outbound.http;
 
+import com.example.similarProducts.domain.model.ProductDetail;
+import com.example.similarProducts.ports.outbound.GetProductDetailPort;
 import com.example.similarProducts.ports.outbound.GetSimilarIdsPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,19 +9,20 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public class SimilarIdsClientAdapter implements GetSimilarIdsPort {
+public class ProductRemoteWebClientAdapter implements GetProductDetailPort {
 
     private final WebClient webClient;
 
-    public SimilarIdsClientAdapter(WebClient webClient) {
+    public ProductRemoteWebClientAdapter(WebClient webClient) {
         this.webClient = webClient;
     }
 
     @Override
-    public Mono<List<String>> getSimilarIds(String productId){
+    public Mono<ProductDetail> getProductDetail(String productId){
         return webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/product/{id}/similarids").build(productId))
+                .uri(uriBuilder -> uriBuilder.path("/product/{id}").build(productId))
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<String>>() {});
+                .bodyToMono(ProductDetail.class);
     }
+
 }
