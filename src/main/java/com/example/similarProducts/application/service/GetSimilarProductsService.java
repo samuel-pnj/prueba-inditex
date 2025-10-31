0b8166039724753c741bbc10dfcc05b084701719
samuel-pnj <samuelperezgamez@outlook.com>
2025-10-31 11:06:21 +0100

    feat(usecase): implement GetSimilarProductsService using reactive approach

---
+++ src/main/java/com/example/similarProducts/application/service/GetSimilarProductsService.java
@@ -0,0 +1,33 @@
+package com.example.similarProducts.application.service;
+
+import com.example.similarProducts.domain.model.ProductDetail;
+import com.example.similarProducts.ports.inbound.GetSimilarProductsPort;
+import com.example.similarProducts.ports.outbound.GetProductDetailPort;
+import com.example.similarProducts.ports.outbound.GetSimilarIdsPort;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+
+@Service
+public class GetSimilarProductsService implements GetSimilarProductsPort {
+    private final GetSimilarIdsPort getSimilarIdsPort;
+    private final GetProductDetailPort getProductDetailPort;
+
+    public GetSimilarProductsService(GetSimilarIdsPort getSimilarIdsPort, GetProductDetailPort getProductDetailPort) {
+        this.getSimilarIdsPort = getSimilarIdsPort;
+        this.getProductDetailPort = getProductDetailPort;
+    }
+
+    @Override
+    public Mono<List<ProductDetail>> getSimilarProducts(String productId){
+        return getSimilarIdsPort.getSimilarIds(productId)
+                .flatMapMany(Flux::fromIterable)
+                .flatMap(id -> getProductDetailPort.getProductDetail(id)
+                        .timeout(Duration.ofSeconds(2))
+                        .onErrorResume(ex -> Mono.empty()))
+                .collectList();
+    }
+}
